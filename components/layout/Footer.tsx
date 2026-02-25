452d6d0ccbbcf30bec764a5fa7176fda891a4b58
ahmed-mhadhbi <ahmed.mhadhbil.75@gmail.com>
2026-02-25 10:29:25 +0100

    vercel bug

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
 
         <div className="border-t border-gray-800 mt-8 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-400 text-sm">
-            © {new Date().getFullYear()} ZinoShop. Tous droits reserves.
+            ï¿½ {new Date().getFullYear()} ZinoShop. Tous droits reserves.
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <Link href="/privacy" className="text-gray-400 hover:text-white text-sm transition-colors">
